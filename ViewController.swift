0fd1e8b559cf99d6b725964c3979b26d848c87c3
TangHongKang <971461935@qq.com>
2017-05-22 21:08:15 +0800

    Geo-BlockingDemo
    location in swift

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Geo-BlockingDemo
+//
+//  Created by tanghongkang on 2017/5/22.
+//  Copyright © 2017年 tanghongkang.cn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
